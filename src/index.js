fdb6a57961e806aeced37d7c477eae154b0c8edb
sholden-cs <sholden@cspace.com>
2018-02-26 05:54:08 -0500

    Progress during Redux lesson 2 - redux at it's core

--- src/index.js
+++ src/index.js
@@ -3,6 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
+import { createStore } from 'redux';
+import reducer from './reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const store = createStore(
+    reducer,
+    window._REDUX_DEVTOOLS_EXTENSION_ && window._REDUX_DEVTOOLS_EXTENSION_()
+)
+
+ReactDOM.render(<App store={store} />, document.getElementById('root'));
 registerServiceWorker();
