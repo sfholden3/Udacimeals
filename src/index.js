7254a4c0136658147b9f7a366ca204377a2f4a36
Shelby Holden <snf2ye@virginia.edu>
2018-02-26 21:13:49 -0500

    Feature: Now importing Feature and using it to pass in store to app

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 import { createStore } from 'redux';
 import reducer from './reducers';
+import { Provider } from 'react-redux';
 
 
 const store = createStore(
@@ -12,5 +13,8 @@ const store = createStore(
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )
 
-ReactDOM.render(<App store={store} />, document.getElementById('root'));
+ReactDOM.render(
+	<Provider store={store}>
+	<App />
+	</Provider>, document.getElementById('root'));
 registerServiceWorker();
