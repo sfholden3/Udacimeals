dacf8f4e65f586d33818aeeb5f51452cd3e1c018
sholden-cs <sholden@cspace.com>
2018-03-03 10:05:16 -0500

    latest class upate

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,19 @@
+import { combineReducers } from 'redux';
 import { ADD_RECIPE, REMOVE_FROM_CALENDAR } from '../actions';
+
+function food(state = {}, action) {
+  switch (action.type) {
+    case ADD_RECIPE:
+      const { recipe } = action;
+
+      return {
+        ...state,
+        [recipe.label]: recipe
+      };
+    default:
+      return state;
+  }
+}
 
 const initialCalendarState = {
   sunday: {
@@ -59,7 +74,7 @@ function calendar(state = initialCalendarState, action) {
         ...state,
         [day]: {
           ...state[day],
-          [meal]: null,
+          [meal]: null
         }
       };
     default:
@@ -67,4 +82,7 @@ function calendar(state = initialCalendarState, action) {
   }
 }
 
-export default calendar
+export default combineReducers({
+  calendar,
+  food
+}) 
